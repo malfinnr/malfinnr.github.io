f5cc270c6ced1e0734a43fb5235f3b715dd8d14a
malfinnr <malfinnr98@gmail.com>
2025-01-04 22:14:06 +0700

    feat: try again

--- portofolio/next.config.ts
+++ portofolio/next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig: NextConfig = {
   output: "export",
-  assetPrefix: isProd ? "/portofolio/" : "",
+  assetPrefix: "/portofolio/",
   images: {
     unoptimized: true,
   },
